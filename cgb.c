ae0da60b79774d1c55c62f91edae04a30e040141
Thomas Bernard <miniupnp@free.fr>
2018-11-28 22:27:48 +0100

    fix expand_maps()

--- cgb.c
+++ cgb.c
@@ -826,23 +826,23 @@ void expand_maps(unsigned char *cgb_map, unsigned char *cgb_atr, unsigned short 
 	unsigned char	*dst, *src;
 
 
-	if(status)															
+	if(status)
 		return;
 
-	columns--;
-
-	for(; columns >= 0; columns--)
+	do
 	{
-		src = &cgb_map[columns * rows];							
+		columns--;
+		src = &cgb_map[columns * rows];
 		dst = &cgb_map[columns * 32];
 		memmove(dst, src, rows);
 		memset((dst + rows), chr, 32 - rows);
 
-		src = &cgb_atr[columns * rows];							
+		src = &cgb_atr[columns * rows];
 		dst = &cgb_atr[columns * 32];
 		memmove(dst, src, rows);
 		memset((dst + rows), 0, 32 - rows);
 	}
+	while (columns > 0);
 }
 
 
