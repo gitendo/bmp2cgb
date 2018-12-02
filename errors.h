1acacd81056285fc42e36bee054a1b8f2186fafe
gitendo <tmk@tuta.io>
2018-12-02 15:12:19 +0100

    removed unused error definitions

--- errors.h
+++ errors.h
@@ -32,24 +32,4 @@ enum {  NO_ERROR,
 		ERR_MALLOC_CGBT
 };
 
-/*
-
-#define ERR_NOT_8BPP	44
-#define ERR_BI_RLE		5
-#define ERR_TOO_BIG		7
-#define ERR_MALLOC_BMIC	8
-#define ERR_MALLOC_TMPC 10
-#define ERR_MALLOC_TMPI	11
-#define	ERR_MALLOC_BMIT	12
-#define	ERR_MAX_TILES	13
-#define ERR_MALLOC_CGBT	14
-#define ERR_PASS1		15
-#define ERR_PASS2		16
-#define ERR_PASS3		17
-#define ERR_PASS4		18
-#define ERR_TMPI_FAILED	19
-#define ERR_UNK_OPTION	20
-#define ERR_WRONG_PAL	21
-#define ERR_PADDING		22
-*/
 #endif
